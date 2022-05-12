5669e074da20ce17f10c8b6edbf4faa653584aa2
Lisset Cuevas Juarez <lisset.cuevas@Lissets-MacBook-Pro.local>
2022-05-12 14:58:16 -0500

    Style Error Message

---
+++ src/components/Helpers/Error.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #ffd0d0e6;
+  padding: 20px 40px 20px 40px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  
+  img{
+    height: 80px;
+    margin: 5px 20px;
+  }
+  
+  h3{
+    margin-top: 0;
+  }
+`

8e21bb55c68f8be4d2ac64f6301a29fa666df268
Lisset Cuevas Juarez <lisset.cuevas@Lissets-MacBook-Pro.local>
2022-05-13 13:39:40 -0500

    Not Found Page

---
+++ src/pages/NotFound.styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const NotFoundContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  
+  img{
+    height: 600px;
+  }
+  
+  h1{
+    margin-bottom: 0px;
+    font-size: xx-large;
+  }
+  
+  h2{
+    margin: 10px 0px 20px 0px;
+  }
+`
